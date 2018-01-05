70f00dd9cb80dc9277d7df68b7ababd87744fbb7
Xavier Coiffard <xavier.coiffard@sellsecure.com>
2018-01-05 13:33:47 -0800

    Fetch the API every 30sec

--- App.js
+++ App.js
@@ -4,23 +4,31 @@ import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 export default class NextBus extends Component {
   constructor(props) {
     super(props);
+	// Setup default value for nextBus
     this.state = {
       nextBus: 'n/a'
     }
   }
   componentDidMount() {
-    return fetch('http://restbus.info/api/agencies/sf-muni/routes/24/stops/4326/predictions')
-      .then((response) => response.json())
-      .then((responseJson) => {
-		this.setState({
-		  nextBus: responseJson[0].values[0].minutes,
-		  next2Bus: responseJson[0].values[1].minutes
-		}, function() {
-		});
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+	// Fetch the API evey 30 secs
+	this.timer = setInterval(()=> this.getBusData(), 30000)
+  }
+
+  async getBusData() {
+	try {
+	  let response = await fetch(
+		'http://restbus.info/api/agencies/sf-muni/routes/24/stops/4326/predictions'
+	  );
+	  let responseJson = await response.json();
+	  //Set the values of the 2 next bus time
+	  this.setState({
+		nextBus: responseJson[0].values[0].minutes,
+		next2Bus: responseJson[0].values[1].minutes
+	  });
+	  return this.setState;
+	} catch (error) {
+	  console.error(error);
+	}
   }
 
   render() {
