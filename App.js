14b9fb1f25e44e06c8a9089cebf12fe6ea4a2b26
Xavier Coiffard <xavier.coiffard@sellsecure.com>
2018-01-05 10:56:52 -0800

    pass the result of the API to the view

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, ListView, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
 export default class NextBus extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nextBus: 'n/a'
+    }
+  }
   componentDidMount() {
     return fetch('http://restbus.info/api/agencies/sf-muni/routes/24/stops/4326/predictions')
       .then((response) => response.json())
       .then((responseJson) => {
-		let nextBus = responseJson[0].values[0].minutes;
+		this.setState({
+		  nextBus: responseJson[0].values[0].minutes,
+		  next2Bus: responseJson[0].values[1].minutes
+		}, function() {
+		});
       })
       .catch((error) => {
         console.error(error);
@@ -14,8 +24,20 @@ export default class NextBus extends Component {
   }
 
   render() {
-    return (
-	  <Text>Hello world! {this.nextBus}</Text>
-    );
+	if (this.state.nextBus) {
+	  return (
+		<View style={{flex: 1, paddingTop: 20}}>
+		<Text style={styles.bigblue}>Next bus 24: {this.state.nextBus}</Text>
+		<Text style={styles.bigblue}>The one after: {this.state.next2Bus}</Text>
+		</View>
+	  );
+	}
   }
 }
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  }
+});
